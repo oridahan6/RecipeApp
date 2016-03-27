28a5b8da4faca7cc9889ccf3a9ee5b17403e0c8b
Ori Dahan <ori.dahan6@gmail.com>
2016-03-27 15:26:25 +0300

    Added recipe model

---
+++ ParseStarterProject/Recipe.swift
@@ -0,0 +1,13 @@
+//
+//  Recipe.swift
+//  Recipes
+//
+//  Created by Ori Dahan on 27/03/2016.
+//  Copyright © 2016 Parse. All rights reserved.
+//
+
+import UIKit
+
+class Recipe: NSObject {
+
+}
