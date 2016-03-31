556bc94e7b7b5b712ee91b5f6071fede8452cbff
Ori Dahan <ori.dahan6@gmail.com>
2016-03-31 10:44:03 +0300

    added Recipe VC title, time, image etc.

--- ParseStarterProject/RecipeViewController.swift
+++ ParseStarterProject/RecipeViewController.swift
@@ -10,12 +10,30 @@ import UIKit
 
 class RecipeViewController: UIViewController {
 
+    @IBOutlet var recipeImageView: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var dateAdded: UILabel!
+    @IBOutlet var level: UILabel!
+    @IBOutlet var overallTime: UILabel!
+    @IBOutlet var type: UILabel!
+    
     var recipe: Recipe!
     
     // MARK: -
     // MARK: View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.titleLabel.text = recipe.title
+        self.type.text = recipe.type
+        self.level.text = recipe.level
+        self.overallTime.text = recipe.getOverallPreperationTimeText()
+        self.dateAdded.text = recipe.getDateAddedDiff()
+        
+        // update image async
+        let imageUrlString = Constants.GDRecipesImagesPath + recipe.imageName
+        Helpers().updateImageFromUrlAsync(imageUrlString, imageViewToUpdate: self.recipeImageView)
+
         
     }
     
