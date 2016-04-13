ad783ef081b6c21942d3d99c821c59fd1487f326
Ori Dahan <ori.dahan6@gmail.com>
2016-04-13 13:57:35 +0300

    added search functionality to favorites

--- ParseStarterProject/FavoritesViewController.swift
+++ ParseStarterProject/FavoritesViewController.swift
@@ -8,16 +8,24 @@
 
 import UIKit
 
-class FavoritesViewController: UITableViewController, SwiftPromptsProtocol {
+class FavoritesViewController: UITableViewController, SwiftPromptsProtocol, UISearchResultsUpdating, UISearchBarDelegate {
 
     let CellIdentifier = "FavoriteRecipeTableViewCell"
     let SegueRecipeViewController = "RecipeViewController"
 
+    var searchButton: UIBarButtonItem!
+    var editButton: UIBarButtonItem!
+    
     var emptyMessageLabel: UILabel?
     
     var recipeRemoved = false
     var prompt = SwiftPromptsView()
     var activityIndicator: ActivityIndicator!
+    
+    var searchController: UISearchController!
+    
+    var filteredRecipes = [Recipe]()
+    var shouldShowSearchResults = false
     
     var recipeIds = [String]()
     var recipes = [Recipe]() {
@@ -46,11 +54,17 @@ class FavoritesViewController: UITableViewController, SwiftPromptsProtocol {
         self.title = getLocalizedString("Favorites")
         
         // Create Edit Button
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: getLocalizedString("edit"), style: .Plain, target: self, action: #selector(FavoritesViewController.editItems(_:)))
-        
+        self.editButton = UIBarButtonItem(title: getLocalizedString("edit"), style: .Plain, target: self, action: #selector(FavoritesViewController.editItems(_:)))
+        navigationItem.leftBarButtonItem = self.editButton
+
+        // Create Search Button
+        self.searchButton = UIBarButtonItem(barButtonSystemItem: .Search, target: self, action: #selector(FavoritesViewController.showSearchBar(_:)))
+        navigationItem.rightBarButtonItem = self.searchButton
+
         // Activity Indicator
         self.activityIndicator = ActivityIndicator(largeActivityView: self.view)
         
+        configureSearchController()
     }
 
     override func didReceiveMemoryWarning() {
@@ -147,6 +161,25 @@ class FavoritesViewController: UITableViewController, SwiftPromptsProtocol {
         self.activityIndicator.hide()
     }
     
+    func showSearchBar(sender: UIBarButtonItem) {
+        self.navigationItem.rightBarButtonItem = nil
+        self.navigationItem.leftBarButtonItem = nil
+        
+        self.navigationItem.titleView = searchController.searchBar
+    }
+    
+    func configureSearchController() {
+        searchController = UISearchController(searchResultsController: nil)
+        searchController.searchResultsUpdater = self
+        searchController.dimsBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = getLocalizedString("searchByName")
+        searchController.searchBar.setValue(getLocalizedString("cancel"), forKey:"_cancelButtonText")
+        searchController.searchBar.delegate = self
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.searchBar.showsCancelButton = true
+        searchController.searchBar.sizeToFit()
+    }
+    
     private func buildAlert() {
         //Create an instance of SwiftPromptsView and assign its delegate
         prompt = SwiftPromptHelper.getSwiftPromptView(self.view.bounds)
@@ -169,7 +202,9 @@ class FavoritesViewController: UITableViewController, SwiftPromptsProtocol {
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if self.recipes.count > 0 {
+        if shouldShowSearchResults {
+            return filteredRecipes.count
+        } else if self.recipes.count > 0 {
             self.tableView.backgroundView = nil
             return recipes.count
         } else if !self.activityIndicator.isAnimating(){
@@ -181,13 +216,18 @@ class FavoritesViewController: UITableViewController, SwiftPromptsProtocol {
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier(CellIdentifier, forIndexPath: indexPath) as! FavoriteRecipeTableViewCell
         
-        let recipe = recipes[indexPath.row]
+        var recipe: Recipe!
+        
+        if shouldShowSearchResults {
+            recipe = filteredRecipes[indexPath.row]
+        } else {
+            recipe = recipes[indexPath.row]
+        }
         
         cell.recipeDetailsView.titleLabel.text = recipe.title
         cell.recipeDetailsView.typeLabel.text = recipe.type
         cell.recipeDetailsView.levelLabel.text = recipe.level
         cell.recipeDetailsView.overallTimeLabel.text = recipe.getOverallPreperationTimeText()
-        
         
         cell.recipeDetailsView.typeImageView.image = recipe.getTypeImage()
         
@@ -232,13 +272,79 @@ class FavoritesViewController: UITableViewController, SwiftPromptsProtocol {
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == SegueRecipeViewController {
             if let indexPath = tableView.indexPathForSelectedRow {
-                let recipe = recipes[indexPath.row]
+                
+                var recipe: Recipe!
+                
+                if shouldShowSearchResults {
+                    recipe = filteredRecipes[indexPath.row]
+                } else {
+                    recipe = recipes[indexPath.row]
+                }
+                
                 let destinationViewController = segue.destinationViewController as! RecipeViewController
                 destinationViewController.recipe = recipe
             }
         }
     }
     
+    //--------------------------------------
+    // MARK: - UISearchBarDelegate methods
+    //--------------------------------------
+
+    func searchBarTextDidEndEditing(searchBar: UISearchBar) {
+        searchController.searchBar.showsCancelButton = true
+    }
+    
+    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
+        shouldShowSearchResults = true
+        tableView.reloadData()
+    }
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+
+        self.navigationItem.rightBarButtonItem = self.searchButton
+        self.navigationItem.leftBarButtonItem = self.editButton
+        
+        self.navigationItem.titleView = nil
+        searchController.searchBar.showsCancelButton = true
+        
+        shouldShowSearchResults = false
+        tableView.reloadData()
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        if !shouldShowSearchResults {
+            shouldShowSearchResults = true
+            tableView.reloadData()
+        }
+        
+        searchController.searchBar.resignFirstResponder()
+    }
+    
+    //--------------------------------------
+    // MARK: - UISearchResultsUpdating methods
+    //--------------------------------------
+
+    func updateSearchResultsForSearchController(searchController: UISearchController) {
+        if let searchString = searchController.searchBar.text where searchString.isEmpty == false {
+        
+            shouldShowSearchResults = true
+            
+            // Filter the data array and get only those countries that match the search text.
+            filteredRecipes = recipes.filter({ (recipe) -> Bool in
+                let recipeTitle: NSString = recipe.title
+                
+                return (recipeTitle.rangeOfString(searchString, options: NSStringCompareOptions.CaseInsensitiveSearch).location) != NSNotFound
+            })
+            
+            // Reload the tableview.
+            tableView.reloadData()
+        } else {
+            shouldShowSearchResults = false
+            tableView.reloadData()
+        }
+    }
+        
     //--------------------------------------
     // MARK: - SwiftPromptsProtocol delegate methods
     //--------------------------------------
