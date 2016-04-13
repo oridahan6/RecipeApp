1a0ddfd7f4d7dab104d10516a522a4a9b1fb2e20
Ori Dahan <ori.dahan6@gmail.com>
2016-04-13 14:56:12 +0300

    added search functionality to recipes view controller

--- ParseStarterProject/RecipesViewController.swift
+++ ParseStarterProject/RecipesViewController.swift
@@ -9,14 +9,21 @@
 import UIKit
 import Kingfisher
 
-class RecipesViewController: UITableViewController, SwiftPromptsProtocol {
+class RecipesViewController: UITableViewController, SwiftPromptsProtocol, UISearchResultsUpdating, UISearchBarDelegate {
 
     let CellIdentifier = "RecipeTableViewCell"
     let SegueRecipeViewController = "RecipeViewController"
     
+    var searchButton: UIBarButtonItem!
+    
     var catId: Int?
     var prompt = SwiftPromptsView()
     var activityIndicator: ActivityIndicator!
+    
+    var searchController: UISearchController!
+    
+    var filteredRecipes = [Recipe]()
+    var shouldShowSearchResults = false
     
     var recipes = [Recipe]() {
         didSet {
@@ -49,7 +56,13 @@ class RecipesViewController: UITableViewController, SwiftPromptsProtocol {
                 ParseHelper().updateRecipes(self)
             }
         }
+        
+        // Create Search Button
+        self.searchButton = UIBarButtonItem(barButtonSystemItem: .Search, target: self, action: #selector(FavoritesViewController.showSearchBar(_:)))
+        navigationItem.rightBarButtonItem = self.searchButton
 
+        configureSearchController()
+        
         // Register Class
         // tableView.registerClass(UITableViewCell.classForCoder(), forCellReuseIdentifier: CellIdentifier)
 
@@ -67,7 +80,14 @@ class RecipesViewController: UITableViewController, SwiftPromptsProtocol {
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == SegueRecipeViewController {
             if let indexPath = tableView.indexPathForSelectedRow {
-                let recipe = recipes[indexPath.row]
+                var recipe: Recipe!
+                
+                if shouldShowSearchResults {
+                    recipe = filteredRecipes[indexPath.row]
+                } else {
+                    recipe = recipes[indexPath.row]
+                }
+                
                 let destinationViewController = segue.destinationViewController as! RecipeViewController
                 destinationViewController.recipe = recipe
             }
@@ -83,13 +103,23 @@ class RecipesViewController: UITableViewController, SwiftPromptsProtocol {
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return recipes.count
+        if shouldShowSearchResults {
+            return filteredRecipes.count
+        } else {
+            return recipes.count
+        }
     }
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier(CellIdentifier, forIndexPath: indexPath) as! RecipeTableViewCell
 
-        let recipe = recipes[indexPath.row]
+        var recipe: Recipe!
+        
+        if shouldShowSearchResults {
+            recipe = filteredRecipes[indexPath.row]
+        } else {
+            recipe = recipes[indexPath.row]
+        }
 
         cell.recipeDetailsView.titleLabel.text = recipe.title
         cell.recipeDetailsView.typeLabel.text = recipe.type
@@ -112,6 +142,24 @@ class RecipesViewController: UITableViewController, SwiftPromptsProtocol {
     // MARK: - Helpers Methods
     //--------------------------------------
     
+    func showSearchBar(sender: UIBarButtonItem) {
+        self.navigationItem.rightBarButtonItem = nil
+        
+        self.navigationItem.titleView = searchController.searchBar
+    }
+    
+    func configureSearchController() {
+        searchController = UISearchController(searchResultsController: nil)
+        searchController.searchResultsUpdater = self
+        searchController.dimsBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = getLocalizedString("searchByName")
+        searchController.searchBar.setValue(getLocalizedString("cancel"), forKey:"_cancelButtonText")
+        searchController.searchBar.delegate = self
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.searchBar.showsCancelButton = true
+        searchController.searchBar.sizeToFit()
+    }
+
     private func buildAlert() {
         //Create an instance of SwiftPromptsView and assign its delegate
         prompt = SwiftPromptHelper.getSwiftPromptView(self.view.bounds)
@@ -125,6 +173,63 @@ class RecipesViewController: UITableViewController, SwiftPromptsProtocol {
         self.view.addSubview(prompt)
     }
 
+    //--------------------------------------
+    // MARK: - UISearchBarDelegate methods
+    //--------------------------------------
+    
+    func searchBarTextDidEndEditing(searchBar: UISearchBar) {
+        searchController.searchBar.showsCancelButton = true
+    }
+    
+    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
+        shouldShowSearchResults = true
+        tableView.reloadData()
+    }
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        
+        self.navigationItem.rightBarButtonItem = self.searchButton
+        
+        self.navigationItem.titleView = nil
+        searchController.searchBar.showsCancelButton = true
+        
+        shouldShowSearchResults = false
+        tableView.reloadData()
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        if !shouldShowSearchResults {
+            shouldShowSearchResults = true
+            tableView.reloadData()
+        }
+        
+        searchController.searchBar.resignFirstResponder()
+    }
+    
+    //--------------------------------------
+    // MARK: - UISearchResultsUpdating methods
+    //--------------------------------------
+    
+    func updateSearchResultsForSearchController(searchController: UISearchController) {
+        if let searchString = searchController.searchBar.text where searchString.isEmpty == false {
+            
+            shouldShowSearchResults = true
+            
+            // Filter the data array and get only those countries that match the search text.
+            filteredRecipes = recipes.filter({ (recipe) -> Bool in
+                let recipeTitle: NSString = recipe.title
+                
+                return (recipeTitle.rangeOfString(searchString, options: NSStringCompareOptions.CaseInsensitiveSearch).location) != NSNotFound
+            })
+            
+            // Reload the tableview.
+            tableView.reloadData()
+        } else {
+            shouldShowSearchResults = false
+            tableView.reloadData()
+        }
+    }
+    
     //--------------------------------------
     // MARK: - SwiftPromptsProtocol delegate methods
     //--------------------------------------
