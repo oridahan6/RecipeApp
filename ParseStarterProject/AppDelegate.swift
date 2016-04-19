bd1a1abed4b71ef87cba7da8c2c16272abf26640
Ori Dahan <ori.dahan6@gmail.com>
2016-04-19 11:57:04 +0300

    organize app customization

--- ParseStarterProject/AppDelegate.swift
+++ ParseStarterProject/AppDelegate.swift
@@ -40,6 +40,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     //--------------------------------------
 
     func customizeApp() {
+
+        // Light status bar color
+        UIApplication.sharedApplication().setStatusBarStyle(UIStatusBarStyle.LightContent, animated: true)
+        
+        self.customizeNavBar()
+        self.customizeTabBar()
+    }
+    
+    func customizeNavBar() {
         // Set Navigation bar background image
         let navBgImage:UIImage = UIImage(named: "navbar.png")!
         UINavigationBar.appearance().setBackgroundImage(navBgImage, forBarMetrics: .Default)
@@ -52,16 +61,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         // Set bar button items font
         UIBarButtonItem.appearance().setTitleTextAttributes([NSFontAttributeName: UIFont(name: "Alef-Regular", size: 18)!], forState: UIControlState.Normal)
-        
-        // Light status bar color
-        UIApplication.sharedApplication().setStatusBarStyle(UIStatusBarStyle.LightContent, animated: true)
-        
+    }
+    
+    func customizeTabBar() {
         // Set Tab bar tint color
         UITabBar.appearance().tintColor = UIColor(red:0.682, green:0.29, blue:0.302, alpha:1)
         
         // Set tab bar items font
         UITabBarItem.appearance().setTitleTextAttributes([NSFontAttributeName: UIFont(name: "Alef-Regular", size: 10)!], forState: UIControlState.Normal)
-
+        
         // Set tab bar items icons and text
         if let tabBarController = self.window?.rootViewController as? UITabBarController {
             let tabBar = tabBarController.tabBar
@@ -69,21 +77,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 let tabBarItem1 = tabBarItems[0]
                 tabBarItem1.title = getLocalizedString("categories")
                 tabBarItem1.image = UIImage.fontAwesomeIconWithName(FontAwesome.ThList, textColor: UIColor.grayColor(), size: CGSizeMake(30, 30))
-
+                
                 let tabBarItem2 = tabBarItems[1]
                 tabBarItem2.title = getLocalizedString("Favorites")
                 tabBarItem2.image = UIImage.fontAwesomeIconWithName(FontAwesome.Heart, textColor: UIColor.grayColor(), size: CGSizeMake(30, 30))
-
+                
                 let tabBarItem3 = tabBarItems[2]
                 tabBarItem3.title = getLocalizedString("Recipes")
                 tabBarItem3.image = UIImage.fontAwesomeIconWithName(FontAwesome.Cutlery, textColor: UIColor.grayColor(), size: CGSizeMake(30, 30))
-
+                
                 let tabBarItem4 = tabBarItems[3]
                 tabBarItem4.title = getLocalizedString("About")
                 tabBarItem4.image = UIImage.fontAwesomeIconWithName(FontAwesome.Info, textColor: UIColor.grayColor(), size: CGSizeMake(35, 35))
             }
         }
-        
     }
     
     //--------------------------------------
