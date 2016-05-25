078d9e79e14705aa3f30a54be1ca539f1f1a6756
Ori Dahan <ori.dahan6@gmail.com>
2016-05-25 11:11:54 +0300

    remove __FUNCTION__ warnings

--- ParseStarterProject/Libraries/TextFieldEffects/TextFieldsEffects.swift
+++ ParseStarterProject/Libraries/TextFieldEffects/TextFieldsEffects.swift
@@ -31,14 +31,14 @@ public class TextFieldEffects : UITextField {
     Creates all the animations that are used to leave the textfield in the "entering text" state.
     */
     public func animateViewsForTextEntry() {
-        fatalError("\(__FUNCTION__) must be overridden")
+        fatalError("\(#function) must be overridden")
     }
     
     /**
     Creates all the animations that are used to leave the textfield in the "display input text" state.
     */
     public func animateViewsForTextDisplay() {
-        fatalError("\(__FUNCTION__) must be overridden")
+        fatalError("\(#function) must be overridden")
     }
     
     /**
@@ -47,11 +47,11 @@ public class TextFieldEffects : UITextField {
     - parameter rect:	The portion of the view’s bounds that needs to be updated.
     */
     public func drawViewsForRect(rect: CGRect) {
-        fatalError("\(__FUNCTION__) must be overridden")
+        fatalError("\(#function) must be overridden")
     }
     
     public func updateViewsForBoundsChange(bounds: CGRect) {
-        fatalError("\(__FUNCTION__) must be overridden")
+        fatalError("\(#function) must be overridden")
     }
     
     // MARK: - Overrides
